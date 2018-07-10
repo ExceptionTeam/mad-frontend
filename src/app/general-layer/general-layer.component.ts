b689b801b50482f9cf6f08f21dc0b8ca04c74343
AnnaKisel <anyakisel17@gmail.com>
2018-07-10 18:58:43 +0300

    created general-layer

---
+++ src/app/general-layer/general-layer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'exc-general-layer',
+  templateUrl: './general-layer.component.html',
+  styleUrls: ['./general-layer.component.scss']
+})
+export class GeneralLayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onMenuToggled() {}
+
+}
