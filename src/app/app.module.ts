eeea99f14c819b8cc76a956af8c5b547d1d8892b
AnnaKisel <anyakisel17@gmail.com>
2018-07-10 17:54:13 +0300

    feat(08) correct imports and removed comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,15 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatSelectModule} from '@angular/material/select';
-import {MatButtonModule} from '@angular/material/button';
-
-
-import {MatIconModule} from '@angular/material/icon';
-
-// import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MatSidenavModule} from '@angular/material/sidenav';
-
-
-import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header.component';
-import {MainMenuComponent} from './main-menu/main-menu.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 import { PersonalMenuComponent } from './personal-menu/personal-menu.component';
 import { DropDownListComponent } from './common/drop-down-list/drop-down-list.component';
 import { FooterComponent } from './footer/footer.component';
