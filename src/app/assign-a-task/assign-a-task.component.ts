7330f62c587d7018435c780bbe9d840f459830fb
AnnaKisel <anyakisel17@gmail.com>
2018-07-12 17:23:02 +0300

    feat(15) added 2 components

---
+++ src/app/assign-a-task/assign-a-task.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'exc-assign-a-task',
+  templateUrl: './assign-a-task.component.html',
+  styleUrls: ['./assign-a-task.component.scss']
+})
+export class AssignATaskComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
