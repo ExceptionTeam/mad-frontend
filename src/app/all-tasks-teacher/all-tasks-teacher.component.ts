4a2df72d537f4a049cdb0d7eb13975a48031fcf1
AnnaKisel <anyakisel17@gmail.com>
2018-07-12 14:46:21 +0300

    feat(15)added input to the table

--- src/app/all-tasks-teacher/all-tasks-teacher.component.ts
+++ src/app/all-tasks-teacher/all-tasks-teacher.component.ts
@@ -1,12 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface TASK {
+  _id: number;
+  name: string;
+  tags: string[];
+  description: string;
+  weight: number;
+}
+
 @Component({
   selector: 'exc-all-tasks-teacher',
   templateUrl: './all-tasks-teacher.component.html',
   styleUrls: ['./all-tasks-teacher.component.scss']
 })
 export class AllTasksTeacherComponent implements OnInit {
-
+  tasks: TASK[] = [
+    { _id: 0, name: 'Триангуляция', tags: ['рек', 'тег', 'тег'], description: 'жыдфвавлаожфыа', weight: 9 },
+    { _id: 12, name: 'Выбрать поднабор', tags: ['рек', 'тег', 'тег'], description: 'жыдфывавлаожфыа', weight: 10 },
+    { _id: 4, name: 'Бинарные последовательности', tags: ['рек', 'тег', 'тег'], description: 'жыдфвавлаожфыа', weight: 10 },
+    { _id: 132, name: 'Пробирки', tags: ['рек', 'тег', 'тег'], description: 'жыдвфвыалаожфыа', weight: 6 },
+    { _id: 152, name: 'Игра', tags: ['рек', 'тег', 'тег'], description: 'жыдвфывалаожфыа', weight: 10 },
+    { _id: 23, name: 'Полигон', tags: ['рек', 'тег', 'тег'], description: 'жыдвлаожфыа', weight: 4 },
+    { _id: 123, name: 'Эпидемия', tags: ['рек', 'тег', 'тег'], description: 'жыдвлаожфыа', weight: 10 },
+    { _id: 123, name: 'Торговые скидки', tags: ['рек', 'тег', 'тег'], description: 'жыдвлаожфыа', weight: 10 },
+    { _id: 123, name: 'Больше не запишешь', tags: ['рек', 'тег', 'тег'], description: 'жыдвлаожфыа', weight: 3 },
+    { _id: 123, name: 'Блок из единиц', tags: ['рек', 'тег', 'тег'], description: 'жыдвлаожфыа', weight: 10 },
+    { _id: 123, name: 'Блок из единиц', tags: ['рек', 'тег', 'тег'], description: 'жыдвлаожфыа', weight: 2 }
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/table-my-tasks-teacher/table-my-tasks-teacher.component.ts
+++ src/app/table-my-tasks-teacher/table-my-tasks-teacher.component.ts
@@ -1,24 +1,4 @@
-import { Component } from '@angular/core';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  date: string;
-  appoint: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Триангуляция n-угольника', date: '11.07.2018', appoint: 'назначить' },
-  { position: 2, name: 'Выбрать поднабор', date: '11.07.2018', appoint: 'назначить' },
-  { position: 3, name: 'Бинарные последовательности', date: '11.07.2018', appoint: 'назначить' },
-  { position: 4, name: 'Пробирки', date: '11.07.2018', appoint: 'назначить' },
-  { position: 5, name: 'Игра', date: '11.07.2018', appoint: 'назначить' },
-  { position: 6, name: 'Полигон', date: '11.07.2018', appoint: 'назначить' },
-  { position: 7, name: 'Эпидемия', date: '11.07.2018', appoint: 'назначить' },
-  { position: 8, name: 'Торговые скидки', date: '11.07.2018', appoint: 'назначить' },
-  { position: 9, name: 'Больше не запишешь', date: '11.07.2018', appoint: 'назначить' },
-  { position: 10, name: 'Блок из единиц', date: '11.07.2018', appoint: 'назначить' },
-];
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'exc-table-my-tasks-teacher',
@@ -26,6 +6,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./table-my-tasks-teacher.component.scss']
 })
 export class TableMyTasksTeacherComponent {
-  displayedColumns: string[] = ['position', 'name', 'date', 'appoint'];
-  dataSource = ELEMENT_DATA;
+
+  @Input() tasks: string[];
+
+  displayedColumns: string[] = ['name', 'weight', 'appoint'];
 }
