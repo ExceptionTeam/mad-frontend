e8965af187e645ec8ddfd3fe527ffc03b1e146e4
AnnaKisel <anyakisel17@gmail.com>
2018-07-17 16:40:13 +0300

    feat(40): created interface for navigation menu and arrays for nav menu fields

--- src/app/layout-landing-page/layout-landing-page.component.ts
+++ src/app/layout-landing-page/layout-landing-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { LayoutNavigationComponent } from '../layout-navigation/layout-navigation.component';
+import { NavigationMenuField } from './layout-landing-page.types';
 
 @Component({
   selector: 'exc-layout-landing-page',
@@ -8,6 +9,32 @@ import { LayoutNavigationComponent } from '../layout-navigation/layout-navigatio
 })
 export class LayoutLandingPageComponent {
   @ViewChild(LayoutNavigationComponent) mainMenu: LayoutNavigationComponent;
+
+  studentNavMenu: NavigationMenuField[] = [
+    { name: 'Мои тесты', link: '' },
+    { name: 'Мои задачи', link: 'tasks-student' },
+    { name: 'Материлы', link: '' },
+  ];
+
+  adminNavMenu: NavigationMenuField[] = [
+    { name: 'Вопросы', link: '' },
+    { name: 'Тесты', link: '' },
+    { name: 'Задачи', link: 'tasks-teacher' },
+    { name: 'Статистика по ученикам', link: '' },
+    { name: 'Статистика по учителям', link: '' },
+    { name: 'Статистика по тестам', link: '' },
+    { name: 'Статистика по задачам', link: '' },
+    { name: 'Лента активности', link: '' },
+    { name: 'Материалы', link: '' },
+  ];
+
+  teacherNavMenu: NavigationMenuField[] = [
+    { name: 'Вопросы', link: '' },
+    { name: 'Тесты', link: '' },
+    { name: 'Задачи', link: 'tasks-teacher' },
+    { name: 'Мои студенты', link: '' },
+    { name: 'Материалы', link: '' },
+  ];
 
   onMenuToggled() {
     this.mainMenu.drawer.toggle();
---
+++ src/app/layout-landing-page/layout-landing-page.types.ts
@@ -0,0 +1,4 @@
+export interface NavigationMenuField {
+  name: string;
+  link: string;
+}
