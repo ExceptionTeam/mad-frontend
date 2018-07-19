2f9b45886a9b02a1d53295e61bf995af8eda2762
OdanetsLiliya <mashcovcheva@gmail.com>
2018-07-19 15:17:19 +0300

    fix(27): changed data in edit-task-page

--- src/app/layout-navigation/layout-navigation.component.ts
+++ src/app/layout-navigation/layout-navigation.component.ts
@@ -1,16 +1,29 @@
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatDrawer } from '@angular/material';
 import { FieldsOfMenu } from '../layout-landing-page/layout-landing-page.types';
+import { InputOutputFiles } from 'src/app/InputOutputFiles.type';
+export interface TaskEditInfo {
+  type: string;
+  editFiles: InputOutputFiles [];
+  name: string;
+  description: string;
+}
 
 @Component({
   selector: 'exc-layout-navigation',
   templateUrl: './layout-navigation.component.html',
   styleUrls: ['./layout-navigation.component.scss'],
 })
+
 export class LayoutNavigationComponent implements OnInit {
   @ViewChild(MatDrawer) drawer: MatDrawer;
   @Input() fields: FieldsOfMenu[];
-
+  TaskEdit: TaskEditInfo = {
+    type: 'отправить',
+    editFiles: null,
+    name: 'ghjkl',
+    description: 'ghjgigoigoiguijgfyvfu'
+  };
   constructor() {
   }
 
--- src/app/task-edit-create-page/task-edit-create-page.component.ts
+++ src/app/task-edit-create-page/task-edit-create-page.component.ts
@@ -1,18 +1,27 @@
 import { Component, Input } from '@angular/core';
 import { InputOutputFiles } from 'src/app/InputOutputFiles.type';
 
+export interface TaskEditInfo {
+  type: string;
+  editFiles: InputOutputFiles [];
+  name: string;
+  description: string;
+}
+
 @Component({
   selector: 'exc-task-edit-create-page',
   templateUrl: './task-edit-create-page.component.html',
   styleUrls: ['./task-edit-create-page.component.scss']
 })
 export class TaskEditPageComponent {
-  validFiles  = true;
-  editFiles = [{input: 'inp1', output: 'out1'}];
-  name = 'Task12';
-  description = 'gyufihshidjshaebfkdshfkeashfoels';
-  InputOutputFiles: InputOutputFiles[] = [];
-
+  // validFiles = true;
+  // editFiles = [{input: 'inp1', output: 'out1'}];
+  // name = 'Task12';
+  // description = 'gyufihshidjshaebfkdshfkeashfoels';
+  // InputOutputFiles: InputOutputFiles[] = [];
+  @Input() validFiles: boolean;
+  @Input() taskInfo: TaskEditInfo;
+  InputOutputFiles: InputOutputFiles [];
   constructor() {
   }
 
@@ -20,7 +29,7 @@ export class TaskEditPageComponent {
     console.log(value.name);
     console.log(value.description);
     console.log(this.InputOutputFiles);
-    console.log(this.editFiles);
+    console.log(this.taskInfo.editFiles);
   }
 
   changeValidState(event) {
@@ -29,7 +38,7 @@ export class TaskEditPageComponent {
   }
 
   onDelete(event, i) {
-    this.editFiles.splice(i, 1);
+    this.taskInfo.editFiles.splice(i, 1);
   }
 
   changeFiles(files) {
@@ -37,6 +46,6 @@ export class TaskEditPageComponent {
   }
 
   changeEditFiles(files) {
-    this.editFiles = files;
+    this.taskInfo.editFiles = files;
   }
 }
