2251c8a28f4ad5112f33d3ad1f17719a64135d43
AnnaKisel <anyakisel17@gmail.com>
2018-07-20 17:26:39 +0300

    feat(59): ceated new service for tasks and a request

--- src/app/task-full-descr-teacher/task-full-descr-teacher.component.ts
+++ src/app/task-full-descr-teacher/task-full-descr-teacher.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { Task } from './task-full-description.types';
+import { Task } from './task-full-description.type';
 import { ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
 import { Location } from '@angular/common';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'exc-task-full-descr-teacher',
@@ -10,33 +11,14 @@ import { Location } from '@angular/common';
   styleUrls: ['./task-full-descr-teacher.component.scss']
 })
 export class TaskFullDescrTeacherComponent {
-  task: Task = {
-    _id: this.activatedRoute.snapshot.params.id,
-    name: 'Название задачи',
-    tags: ['тег', 'тег', 'тег'],
-    description: 'Задана система дорог, определяемая набором пар городов. Каждая такая пара { i, j } указывает, ' +
-    'что из города i можно проехать в город j и в обратном направлении. Необходимо определить, можно ли проехать из заданного ' +
-    'города a в заданный город b таким образом, чтобы посетить город c, не проезжать ни по какой дороге более одного раза ' +
-    'и не заезжать ни в какой город более одного раза.',
-    weight: 10,
-    inputFilesUrls: [{ link: 'https://pp.userapi.com/c845523/v845523795/91587/8MVvxQHQ8yA.jpg', name: 'input1.txt' },
-      { link: 'https://pp.userapi.com/c845523/v845523795/91587/8MVvxQHQ8yA.jpg', name: 'input2.txt' },
-      { link: 'https://sun9-4.userapi.com/c7001/v7001950/4b499/eAKxv9RVmrQ.jpg', name: 'input3.txt' },
-      { link: 'https://sun9-4.userapi.com/c7001/v7001950/4b499/eAKxv9RVmrQ.jpg', name: 'input4.txt' },
-      { link: 'https://sun9-4.userapi.com/c7001/v7001950/4b499/eAKxv9RVmrQ.jpg', name: 'input5.txt' },
-    ],
-    outputFilesUrls: [{ link: 'https://pp.userapi.com/c845523/v845523795/91587/8MVvxQHQ8yA.jpg', name: 'ouput1.txt' },
-      { link: 'https://pp.userapi.com/c845523/v845523795/91587/8MVvxQHQ8yA.jpg', name: 'output2.txt' },
-      { link: 'https://pp.userapi.com/c845523/v845523795/91587/8MVvxQHQ8yA.jpg', name: 'output3.txt' },
-      { link: 'https://pp.userapi.com/c845523/v845523795/91587/8MVvxQHQ8yA.jpg', name: 'output4.txt' },
-      { link: 'https://pp.userapi.com/c845523/v845523795/91587/8MVvxQHQ8yA.jpg', name: 'output5.txt' },
-    ],
-    additAbility: true
-  };
+  task: Partial<Task>;
 
-  constructor(private activatedRoute: ActivatedRoute, private location: Location, public snackBar: MatSnackBar) {
+  constructor(private activatedRoute: ActivatedRoute,
+              private location: Location,
+              public snackBar: MatSnackBar,
+              private taskService: TaskService) {
     const id = this.activatedRoute.snapshot.params.id;
-    console.log('id: ' + id);
+    this.taskService.getTeacherFullDescription(id).subscribe(task => this.task = task);
   }
 
   openSnackbar() {
