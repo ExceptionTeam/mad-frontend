79e6ed6942ea4dbda1511cda737a0abf8ec23a86
AnnaKisel <anyakisel17@gmail.com>
2018-07-24 17:20:21 +0300

    feat(86): clean code

--- src/app/task-edit-create-page/task-edit-create-page.component.ts
+++ src/app/task-edit-create-page/task-edit-create-page.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { InputOutputFiles } from 'src/app/Types/InputOutputFiles.type';
 import { TaskFullInfo } from 'src/app/Types/TaskFullInfo.type';
-import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { TaskService } from 'src/app/task.service';
 
@@ -18,9 +17,8 @@ export class TaskEditCreatePageComponent {
   marks = [4, 5, 6, 7, 8, 9, 10];
   private id;
 
-  constructor(private activatedRoute: ActivatedRoute, private location: Location,
-              private tasksService: TaskService
-  ) {
+  constructor(private activatedRoute: ActivatedRoute,
+              private tasksService: TaskService) {
     this.validFiles = this.validFiles || false;
     this.taskInfo = this.taskInfo || {
       name: '',
