5368cb78b2bb63298be64e8b01a771275f214ffc
AnnaKisel <anyakisel17@gmail.com>
2018-07-24 12:17:01 +0300

    fix(48): service fixed

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -8,40 +8,37 @@ import { TaskFullDStudent } from './task-sending-page/task-sending-page.type';
   providedIn: 'root'
 })
 export class TaskService {
+  private headers;
+
   constructor(private http: HttpClient) {
+    this.headers = new HttpHeaders();
+    this.headers.append('Access-Control-Allow-Headers', 'Content-Type');
+    this.headers.append('Access-Control-Allow-Origin', 'http://localhost:4200');
   }
 
   getTeacherFullDescription(id: string): Observable<Task> {
     const headers = new HttpHeaders();
-    headers.append('Access-Control-Allow-Headers', 'Content-Type');
     headers.append('Access-Control-Allow-Methods', 'GET');
-    headers.append('Access-Control-Allow-Origin', 'http://localhost:4200');
     console.log(`http://localhost:3000/teacher/task/full-info/${id}`);
     return this.http.get<Task>(`http://localhost:3000/teacher/task/full-info/${id}`, { headers: headers });
   }
 
   getStudentFullDescription(assId: string): Observable<TaskFullDStudent> {
     const headers = new HttpHeaders();
-    headers.append('Access-Control-Allow-Headers', 'Content-Type');
     headers.append('Access-Control-Allow-Methods', 'GET');
-    headers.append('Access-Control-Allow-Origin', 'http://localhost:4200');
     console.log(`http://localhost:3000/student/task/full-info/${assId}`);
     return this.http.get<TaskFullDStudent>(`http://localhost:3000/student/task/full-info/${assId}`, { headers: headers });
   }
 
   deleteTask(id: string): Observable<boolean> {
     const headers = new HttpHeaders();
-    headers.append('Access-Control-Allow-Headers', 'Content-Type');
-    headers.append('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    headers.append('Access-Control-Allow-Origin', 'http://localhost:4200');
+    headers.append('Access-Control-Allow-Methods', 'DELETE');
     return this.http.delete<boolean>(`http://localhost:3000/teacher/task/delete/${id}`, { headers: headers });
   }
 
   activateTask(id: string): Observable<any> {
     const headers = new HttpHeaders();
-    headers.append('Access-Control-Allow-Headers', 'Content-Type');
     headers.append('Access-Control-Allow-Methods', 'POST');
-    headers.append('Access-Control-Allow-Origin', 'http://localhost:4200');
     return this.http.post<any>(`http://localhost:3000/admin/task/activate/${id}`, {}, { headers: headers });
   }
 }
