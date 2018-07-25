7ab504dbd7f546fc0bacb59771593911517985e7
AnnaKisel <anyakisel17@gmail.com>
2018-07-25 18:46:42 +0300

    feat(96): clean code

--- src/app/edit-password/edit-password.module.ts
+++ src/app/edit-password/edit-password.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
-import { EditPasswordComponent} from './edit-password.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {MatIconModule} from '@angular/material';
-import {MatFormFieldModule} from '@angular/material';
+import { EditPasswordComponent } from './edit-password.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatDividerModule, MatFormFieldModule, MatIconModule, MatTooltipModule } from '@angular/material';
 import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material';
-import { MatDividerModule} from '@angular/material';
-import { MatTooltipModule} from '@angular/material';
 
 @NgModule({
   imports: [
