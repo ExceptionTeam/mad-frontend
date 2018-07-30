3dc8e963515bcd8aef9665aabd529395157a7ebe
AnnaKisel <anyakisel17@gmail.com>
2018-07-30 17:34:32 +0300

    feat(121): some edits in structure

--- src/app/notifications-admin/notifications-admin.component.ts
+++ src/app/notifications-admin/notifications-admin.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notifications-admin.component.scss']
 })
 export class NotificationsAdminComponent implements OnInit {
-  role = 'teacher';
+  role = 'admin';
   links;
 
   navLinksAdmin = [
