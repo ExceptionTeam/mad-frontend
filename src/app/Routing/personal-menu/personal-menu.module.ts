30004ccd8c898e5426d3914b654d4386cbfe6f38
AnnaKisel <anyakisel17@gmail.com>
2018-07-30 22:20:45 +0300

    feat(132): clean code

--- src/app/Routing/personal-menu/personal-menu.module.ts
+++ src/app/Routing/personal-menu/personal-menu.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { PersonalMenuRoutingModule} from './personal-menu-routing.module';
+import { PersonalMenuRoutingModule } from './personal-menu-routing.module';
 import { MyGroupsTeacherPageModule } from '../../my-groups-teacher-page/my-groups-teacher-page.module';
 import { NotificationsAdminModule } from '../../notifications-admin/notifications-admin.module';
 import { NotificationsPageStudentModule } from '../../notifications-page-student/notifications-page-student.module';
