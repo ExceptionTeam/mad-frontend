866c03daadd53f1033fcb956e86d8177410f69de
vladmalinovsky <vlad.malinovsky.98@gmail.com>
2018-08-01 16:40:44 +0300

    Routing added to assign test page

--- src/app/Routing/test/test.module.ts
+++ src/app/Routing/test/test.module.ts
@@ -3,13 +3,15 @@ import { TestStatisticsUsersPageModule } from '../../test-statistics-users-page/
 import { TestStatisticsTestsPageModule } from '../../test-statistics-tests-page/test-statistics-tests-page.module';
 import { TestRoutingModule } from './test-routing.module';
 import { TestPassingPageModule } from '../../test-passing-page/test-passing-page.module';
+import { TestAssignModule } from '../../test-assign-page/test-assign-page.module';
 
 @NgModule({
   imports: [
     TestStatisticsUsersPageModule,
     TestStatisticsTestsPageModule,
     TestRoutingModule,
-    TestPassingPageModule
+    TestPassingPageModule,
+    TestAssignModule
   ],
   exports: [],
   declarations: [],
