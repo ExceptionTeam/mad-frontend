994fbe173107b96e3d68cdde4e0a5a6416daa76b
vladmalinovsky <vlad.malinovsky.98@gmail.com>
2018-08-06 16:11:58 +0300

    Feat(169): created request for adding question

--- src/app/test.service.ts
+++ src/app/test.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { QuestionAdmin } from './Types/QuestionAdmin.type';
+import { QuestionAdding } from './Types/QuestionAdding.type';
 
 const questionsAdmin: QuestionAdmin[] = [
   {question: 'Что такое инкапсуляция: я ничего не знаю и знать не собираюсь, вот так вот', tags: ['ООП', 'Инкапсуляция'], status: true},
@@ -21,6 +23,31 @@ export class TestService {
     pageSize: 5,
     length: questionsAdmin.length
   };
+  private headers;
+  public que: QuestionAdding = {
+  section: [],
+  tags: [],
+  type: '',
+  active: true,
+  category: '',
+  question: '',
+  questionAuthorId: '',
+  answerOptions: [],
+  correctOptions: [],
+  difficulty: 1
+  };
+  constructor(private http: HttpClient) {
+    this.headers = new HttpHeaders();
+    this.headers.append('Access-Control-Allow-Headers', 'Content-Type');
+    this.headers.append('Access-Control-Allow-Origin', 'http://localhost:4200');
+  }
+
+  postAddQuestion (): Observable<QuestionAdding> {
+    this.headers.append('Access-Control-Allow-Methods', 'POST');
+    console.log(this.que );
+    return this.http.post<QuestionAdding>('http://localhost:3000/teacher/test/new-question/',
+     this.que, { headers: this.headers, withCredentials: true });
+  }
 
   loadQuestions(): Observable<QuestionAdmin[]> {
     const startIndex = this.paginationParams.pageIndex * this.paginationParams.pageSize;
